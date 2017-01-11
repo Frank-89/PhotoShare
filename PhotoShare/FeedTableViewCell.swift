2ce5ef95cc6b3de16a8a0618b4987d092ca3ee79
HAOYU WU <Haoyu@MyMac.local>
2017-01-10 23:28:01 -0800

    PhotoShare_First commit

---
+++ PhotoShare/FeedTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  FeedTableViewCell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by HAOYU WU on 12/31/16.
+//  Copyright © 2016 Haoyu. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var usernameLabel: UILabel!
+    
+    @IBOutlet var messageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
